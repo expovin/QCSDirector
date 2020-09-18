3529d0b6612bfd397164bdf164f7a728223dbd5c
Vincenzo <expovin@gmail.com>
2020-09-18 17:04:27 +0200

    First commit express version

---
+++ routes/api.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const db = require('../lib/db');
+const router = express.Router();
+
+let repo = new db();
+
+/* Working on QCS */
+router.route('/qcssite')
+.get( (req, res, next) => {
+  repo.getAllQCSSite()
+  .then( result => {
+      res.status(200).json({success: true, data : result})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+.post( (req, res, next) => {
+  repo.addQCSSite(req.body)
+  .then( result => {
+      res.status(200).json({success: true, data : result})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+
+router.route('/qcssite/:id')
+.get( (req, res, next) => {
+  repo.getQCSSiteById(req.params.id)
+  .then( result => {
+      res.status(200).json({success: true, data : result})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+.put( (req, res, next) => {
+  repo.delQCSSiteById(req.params.id)
+  .then( result => {
+      return repo.addQCSSite(req.body)
+  })
+  .then( result => {
+    res.status(200).json({success: true, data : result})
+})  
+  .catch(e => res.status(500).json({success: false, error : e}))
+})
+.delete( (req, res, next) => {
+  repo.delQCSSiteById(req.params.id)
+  .then( result => {
+      res.status(200).json({success: true, data : result})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+
+
+
+/* Working on Sequences */
+router.route('/sequence')
+.get( (req, res, next) => {
+  repo.getAllSequences()
+  .then( result => {
+      res.status(200).json({success: true, data : result})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+.post( (req, res, next) => {
+  repo.addSequence(req.body)
+  .then( result => {
+      res.status(200).json({success: true, data : result})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+
+router.route('/sequence/:id')
+.get( (req, res, next) => {
+  repo.getQCSSequenceById(req.params.id)
+  .then( result => {
+      res.status(200).json({success: true, data : result[0]})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+.put( (req, res, next) => {
+  repo.delSequenceById(req.params.id)
+  .then( result => {
+      return repo.addSequence(req.body)
+  })
+  .then( result => {
+    res.status(200).json({success: true, data : result})
+})  
+  .catch(e => res.status(500).json({success: false, error : e}))
+})
+.delete( (req, res, next) => {
+  repo.delSequenceById(req.params.id)
+  .then( result => {
+      res.status(200).json({success: true, data : result})
+  }).catch(e => res.status(500).json({success: false, error : e}))
+})
+
+module.exports = router;
